564e5697bd907ddaea56b9b32b11c6de0c0d5762
ywkuo227 <ywkuo@outlook.com>
2021-08-15 00:18:01 -0700

    Aug 15th code update #1: - Installed and referenced dependencies. - Completed Models and Seed files - Routes are WIP: Still need to work on commentRoutes and homeRoutes. - TO-DO: Need to work on Views and scripts under Public.

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const { Post } = require("../../models");
+// const withAuth = require("../../utils/auth");
+
+router.post("/", async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        })
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        })
+
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this ID."});
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
