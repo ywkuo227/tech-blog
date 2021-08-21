541113fc30b9fceaf9a2f8576d213a7257d1bba1
ywkuo227 <ywkuo@outlook.com>
2021-08-21 12:21:57 -0700

    Aug 21st code update #1: - Removal of maxAge from cookie setting in server.js due to bug.

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 3600
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
